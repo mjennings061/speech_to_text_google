c0cda5921383a26c1aa444d9ff5c80c988318e72
Michael Jennings <mjennings061@gmail.com>
2021-04-04 00:06:28 +0100

    Added GoogleSpeech class

--- gcloud.py
+++ gcloud.py
@@ -1,7 +1,9 @@
+import os
 import time
 import re
 import sys
 
+from google.cloud import speech
 import pyaudio
 from six.moves import queue
 
@@ -15,10 +17,134 @@ RED = "\033[0;31m"
 GREEN = "\033[0;32m"
 YELLOW = "\033[0;33m"
 
-def get_current_time():
-    """Return Current Time in MS."""
 
-    return int(round(time.time() * 1000))
+class GoogleSpeech:
+    def __init__(self):
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/mj/PycharmProjects/speech-309618-0f83735de272.json"
+        self.client = speech.SpeechClient()
+        self.config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=SAMPLE_RATE,
+            language_code="en-US",
+            max_alternatives=1,
+        )
+
+        self.streaming_config = speech.StreamingRecognitionConfig(
+            config=self.config, interim_results=True
+        )
+
+        sys.stdout.write(YELLOW)
+        sys.stdout.write('\nListening, say "Quit" or "Exit" to stop.\n\n')
+        sys.stdout.write("End (ms)       Transcript Results/Status\n")
+        sys.stdout.write("=====================================================\n")
+
+    def get_current_time(self):
+        """Return Current Time in MS."""
+        return int(round(time.time() * 1000))
+
+    def listen_print_loop(self, responses, stream):
+        """Iterates through server responses and prints them.
+        The responses passed is a generator that will block until a response
+        is provided by the server.
+        Each response may contain multiple results, and each result may contain
+        multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+        print only the transcription for the top alternative of the top result.
+        In this case, responses are provided for interim results as well. If the
+        response is an interim one, print a line feed at the end of it, to allow
+        the next result to overwrite it, until the response is a final one. For the
+        final one, print a newline to preserve the finalized transcription.
+        """
+
+        for response in responses:
+
+            if self.get_current_time() - stream.start_time > STREAMING_LIMIT:
+                stream.start_time = self.get_current_time()
+                break
+
+            if not response.results:
+                continue
+
+            result = response.results[0]
+
+            if not result.alternatives:
+                continue
+
+            transcript = result.alternatives[0].transcript
+
+            result_seconds = 0
+            result_micros = 0
+
+            if result.result_end_time.seconds:
+                result_seconds = result.result_end_time.seconds
+
+            if result.result_end_time.microseconds:
+                result_micros = result.result_end_time.microseconds
+
+            stream.result_end_time = int((result_seconds * 1000) + (result_micros / 1000))
+
+            corrected_time = (
+                    stream.result_end_time
+                    - stream.bridging_offset
+                    + (STREAMING_LIMIT * stream.restart_counter)
+            )
+            # Display interim results, but with a carriage return at the end of the
+            # line, so subsequent lines will overwrite them.
+
+            if result.is_final:
+
+                sys.stdout.write(GREEN)
+                sys.stdout.write("\033[K")
+                sys.stdout.write(str(corrected_time) + ": " + transcript + "\n")
+
+                stream.is_final_end_time = stream.result_end_time
+                stream.last_transcript_was_final = True
+
+                # Exit recognition if any of the transcribed phrases could be
+                # one of our keywords.
+                if re.search(r"\b(exit|quit)\b", transcript, re.I):
+                    sys.stdout.write(YELLOW)
+                    sys.stdout.write("Exiting...\n")
+                    stream.closed = True
+                    return transcript
+
+            else:
+                sys.stdout.write(RED)
+                sys.stdout.write("\033[K")
+                sys.stdout.write(str(corrected_time) + ": " + transcript + "\r")
+
+                stream.last_transcript_was_final = False
+
+    def transcript_in_loop(self, stream):
+        while not stream.closed:
+            sys.stdout.write(YELLOW)
+            sys.stdout.write(
+                "\n" + str(STREAMING_LIMIT * stream.restart_counter) + ": NEW REQUEST\n"
+            )
+
+            stream.audio_input = []
+            audio_generator = stream.generator()
+
+            requests = (
+                speech.StreamingRecognizeRequest(audio_content=content)
+                for content in audio_generator
+            )
+
+            responses = self.client.streaming_recognize(self.streaming_config, requests)
+
+            # Now, put the transcription responses to use.
+            transcript = self.listen_print_loop(responses, stream)
+
+            if stream.result_end_time > 0:
+                stream.final_request_end_time = stream.is_final_end_time
+            stream.result_end_time = 0
+            stream.last_audio_input = []
+            stream.last_audio_input = stream.audio_input
+            stream.audio_input = []
+            stream.restart_counter = stream.restart_counter + 1
+
+            if not stream.last_transcript_was_final:
+                sys.stdout.write("\n")
+            stream.new_stream = True
 
 
 class ResumableMicrophoneStream:
@@ -30,7 +156,7 @@ class ResumableMicrophoneStream:
         self._num_channels = 1
         self._buff = queue.Queue()
         self.closed = True
-        self.start_time = get_current_time()
+        self.start_time = self.get_current_time()
         self.restart_counter = 0
         self.audio_input = []
         self.last_audio_input = []
@@ -130,75 +256,6 @@ class ResumableMicrophoneStream:
 
             yield b"".join(data)
 
-
-def listen_print_loop(responses, stream):
-    """Iterates through server responses and prints them.
-    The responses passed is a generator that will block until a response
-    is provided by the server.
-    Each response may contain multiple results, and each result may contain
-    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-    print only the transcription for the top alternative of the top result.
-    In this case, responses are provided for interim results as well. If the
-    response is an interim one, print a line feed at the end of it, to allow
-    the next result to overwrite it, until the response is a final one. For the
-    final one, print a newline to preserve the finalized transcription.
-    """
-
-    for response in responses:
-
-        if get_current_time() - stream.start_time > STREAMING_LIMIT:
-            stream.start_time = get_current_time()
-            break
-
-        if not response.results:
-            continue
-
-        result = response.results[0]
-
-        if not result.alternatives:
-            continue
-
-        transcript = result.alternatives[0].transcript
-
-        result_seconds = 0
-        result_micros = 0
-
-        if result.result_end_time.seconds:
-            result_seconds = result.result_end_time.seconds
-
-        if result.result_end_time.microseconds:
-            result_micros = result.result_end_time.microseconds
-
-        stream.result_end_time = int((result_seconds * 1000) + (result_micros / 1000))
-
-        corrected_time = (
-            stream.result_end_time
-            - stream.bridging_offset
-            + (STREAMING_LIMIT * stream.restart_counter)
-        )
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
-
-        if result.is_final:
-
-            sys.stdout.write(GREEN)
-            sys.stdout.write("\033[K")
-            sys.stdout.write(str(corrected_time) + ": " + transcript + "\n")
-
-            stream.is_final_end_time = stream.result_end_time
-            stream.last_transcript_was_final = True
-
-            # Exit recognition if any of the transcribed phrases could be
-            # one of our keywords.
-            if re.search(r"\b(exit|quit)\b", transcript, re.I):
-                sys.stdout.write(YELLOW)
-                sys.stdout.write("Exiting...\n")
-                stream.closed = True
-                break
-
-        else:
-            sys.stdout.write(RED)
-            sys.stdout.write("\033[K")
-            sys.stdout.write(str(corrected_time) + ": " + transcript + "\r")
-
-            stream.last_transcript_was_final = False
+    def get_current_time(self):
+        """Return Current Time in MS."""
+        return int(round(time.time() * 1000))
--- main.py
+++ main.py
@@ -25,73 +25,18 @@ Example usage:
 
 # [START speech_transcribe_infinite_streaming]
 
-import re
 import sys
-import time
-import os
-
-from google.cloud import speech
-import pyaudio
-from six.moves import queue
 import gcloud
-from gcloud import listen_print_loop, ResumableMicrophoneStream
+from gcloud import GoogleSpeech, ResumableMicrophoneStream
 
 
 def main():
     """start bidirectional streaming from microphone input to speech API"""
 
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/mj/PycharmProjects/speech-309618-0f83735de272.json"
-    client = speech.SpeechClient()
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=gcloud.SAMPLE_RATE,
-        language_code="en-US",
-        max_alternatives=1,
-    )
-
-    streaming_config = speech.StreamingRecognitionConfig(
-        config=config, interim_results=True
-    )
-
+    speech = GoogleSpeech()
     mic_manager = ResumableMicrophoneStream(gcloud.SAMPLE_RATE, gcloud.CHUNK_SIZE)
-    print(mic_manager.chunk_size)
-    sys.stdout.write(gcloud.YELLOW)
-    sys.stdout.write('\nListening, say "Quit" or "Exit" to stop.\n\n')
-    sys.stdout.write("End (ms)       Transcript Results/Status\n")
-    sys.stdout.write("=====================================================\n")
-
     with mic_manager as stream:
-
-        while not stream.closed:
-            sys.stdout.write(gcloud.YELLOW)
-            sys.stdout.write(
-                "\n" + str(gcloud.STREAMING_LIMIT * stream.restart_counter) + ": NEW REQUEST\n"
-            )
-
-            stream.audio_input = []
-            audio_generator = stream.generator()
-
-            requests = (
-                speech.StreamingRecognizeRequest(audio_content=content)
-                for content in audio_generator
-            )
-
-            responses = client.streaming_recognize(streaming_config, requests)
-
-            # Now, put the transcription responses to use.
-            listen_print_loop(responses, stream)
-
-            if stream.result_end_time > 0:
-                stream.final_request_end_time = stream.is_final_end_time
-            stream.result_end_time = 0
-            stream.last_audio_input = []
-            stream.last_audio_input = stream.audio_input
-            stream.audio_input = []
-            stream.restart_counter = stream.restart_counter + 1
-
-            if not stream.last_transcript_was_final:
-                sys.stdout.write("\n")
-            stream.new_stream = True
+        speech.transcript_in_loop(stream)
 
 
 if __name__ == "__main__":
