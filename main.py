5611a67ec642837102a28e3fd7a7e817c2b15531
Michael Jennings <mjennings061@gmail.com>
2021-04-09 23:25:12 +0100

    Save transcripts to a txt file and waveform to csv

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ import sys
 import gcloud
 from gcloud import GoogleSpeech, ResumableMicrophoneStream
 import matplotlib.pyplot as plt
+import struct
+import numpy as np
+import xlsxwriter as xlsw
+from scipy.io import wavfile
 
 
 def main():
@@ -23,13 +27,25 @@ def main():
     with mic_manager as stream:
         transcripts = speech.transcript_in_loop(stream)     # return all detected speech as a list
 
-    # for chunk in stream.last_audio_input:
-    #     audio_int = int.from_bytes(chunk, byteorder='big')
-    #     print(chunk)
-    #     print(audio_int)
-    #     audio_int = [int.from_bytes(sample, byteorder='big') for sample in chunk]
-    # plt.plot(audio_int)
-    # plt.show()
+    # save the transcribed data to a txt file
+    transcripts_str = '\n'.join(transcripts)    # split by newlines
+    with open('output/transcripts.txt', 'w') as f:
+        f.write(transcripts_str)
+
+    # combine all waveforms (chunks) into one variable and save to a csv file
+    data_int = []
+    for chunk in stream.last_audio_input:
+        # convert data to integers, make np array, then offset it by 127
+        data_int.extend(struct.unpack(str(2 * gcloud.CHUNK_SIZE) + 'B', chunk)[::2])
+
+    data_int = [i - 127 for i in data_int]
+    data_np = np.asarray(data_int, dtype=np.int16)
+    np.savetxt('output/data_np.csv', data_np, delimiter=',')
+
+    # plot a 0.5s of the waveform file
+    plt.ion()
+    plt.plot(data_np[0:6000])
+    plt.show()
 
 
 if __name__ == "__main__":
