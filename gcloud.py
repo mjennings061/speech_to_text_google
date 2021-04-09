2d70db85373453e8d542cad4a1dc1f2e08143b71
Michael Jennings <mjennings061@gmail.com>
2021-04-09 20:20:32 +0100

    Return transcripts from recogniser into main()

--- gcloud.py
+++ gcloud.py
@@ -7,7 +7,6 @@ from google.cloud import speech
 import pyaudio
 from six.moves import queue
 
-
 # Audio recording parameters
 STREAMING_LIMIT = 240000  # 4 minutes
 SAMPLE_RATE = 16000
@@ -16,6 +15,11 @@ CHUNK_SIZE = int(SAMPLE_RATE / 10)  # 100ms
 RED = "\033[0;31m"
 GREEN = "\033[0;32m"
 YELLOW = "\033[0;33m"
+
+
+def get_current_time():
+    """Return Current Time in MS."""
+    return int(round(time.time() * 1000))
 
 
 class GoogleSpeech:
@@ -38,10 +42,6 @@ class GoogleSpeech:
         sys.stdout.write("End (ms)       Transcript Results/Status\n")
         sys.stdout.write("=====================================================\n")
 
-    def get_current_time(self):
-        """Return Current Time in MS."""
-        return int(round(time.time() * 1000))
-
     def listen_print_loop(self, responses, stream):
         """Iterates through server responses and prints them.
         The responses passed is a generator that will block until a response
@@ -55,10 +55,11 @@ class GoogleSpeech:
         final one, print a newline to preserve the finalized transcription.
         """
 
+        transcripts = []
         for response in responses:
 
-            if self.get_current_time() - stream.start_time > STREAMING_LIMIT:
-                stream.start_time = self.get_current_time()
+            if get_current_time() - stream.start_time > STREAMING_LIMIT:
+                stream.start_time = get_current_time()
                 break
 
             if not response.results:
@@ -98,6 +99,7 @@ class GoogleSpeech:
 
                 stream.is_final_end_time = stream.result_end_time
                 stream.last_transcript_was_final = True
+                transcripts.append(transcript)  # save the transcribed audio
 
                 # Exit recognition if any of the transcribed phrases could be
                 # one of our keywords.
@@ -105,14 +107,13 @@ class GoogleSpeech:
                     sys.stdout.write(YELLOW)
                     sys.stdout.write("Exiting...\n")
                     stream.closed = True
-                    return transcript
 
             else:
                 sys.stdout.write(RED)
                 sys.stdout.write("\033[K")
                 sys.stdout.write(str(corrected_time) + ": " + transcript + "\r")
-
                 stream.last_transcript_was_final = False
+        return transcripts
 
     def transcript_in_loop(self, stream):
         while not stream.closed:
@@ -132,7 +133,7 @@ class GoogleSpeech:
             responses = self.client.streaming_recognize(self.streaming_config, requests)
 
             # Now, put the transcription responses to use.
-            transcript = self.listen_print_loop(responses, stream)
+            transcripts = self.listen_print_loop(responses, stream)
 
             if stream.result_end_time > 0:
                 stream.final_request_end_time = stream.is_final_end_time
@@ -141,10 +142,12 @@ class GoogleSpeech:
             stream.last_audio_input = stream.audio_input
             stream.audio_input = []
             stream.restart_counter = stream.restart_counter + 1
+            stream.new_stream = True
 
             if not stream.last_transcript_was_final:
                 sys.stdout.write("\n")
-            stream.new_stream = True
+
+        return transcripts
 
 
 class ResumableMicrophoneStream:
@@ -156,7 +159,7 @@ class ResumableMicrophoneStream:
         self._num_channels = 1
         self._buff = queue.Queue()
         self.closed = True
-        self.start_time = self.get_current_time()
+        self.start_time = get_current_time()
         self.restart_counter = 0
         self.audio_input = []
         self.last_audio_input = []
@@ -180,12 +183,10 @@ class ResumableMicrophoneStream:
         )
 
     def __enter__(self):
-
         self.closed = False
         return self
 
     def __exit__(self, type, value, traceback):
-
         self._audio_stream.stop_stream()
         self._audio_stream.close()
         self.closed = True
@@ -255,7 +256,3 @@ class ResumableMicrophoneStream:
                     break
 
             yield b"".join(data)
-
-    def get_current_time(self):
-        """Return Current Time in MS."""
-        return int(round(time.time() * 1000))
--- main.py
+++ main.py
@@ -1,19 +1,4 @@
 
-
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """Google Cloud Speech API sample application using the streaming API.
 NOTE: This module requires the dependencies `pyaudio` and `termcolor`.
 To install using pip:
@@ -23,11 +8,11 @@ Example usage:
     python transcribe_streaming_infinite.py
 """
 
-# [START speech_transcribe_infinite_streaming]
 
 import sys
 import gcloud
 from gcloud import GoogleSpeech, ResumableMicrophoneStream
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -36,10 +21,16 @@ def main():
     speech = GoogleSpeech()
     mic_manager = ResumableMicrophoneStream(gcloud.SAMPLE_RATE, gcloud.CHUNK_SIZE)
     with mic_manager as stream:
-        speech.transcript_in_loop(stream)
+        transcripts = speech.transcript_in_loop(stream)     # return all detected speech as a list
+
+    # for chunk in stream.last_audio_input:
+    #     audio_int = int.from_bytes(chunk, byteorder='big')
+    #     print(chunk)
+    #     print(audio_int)
+    #     audio_int = [int.from_bytes(sample, byteorder='big') for sample in chunk]
+    # plt.plot(audio_int)
+    # plt.show()
 
 
 if __name__ == "__main__":
     main()
-
-# [END speech_transcribe_infinite_streaming]
